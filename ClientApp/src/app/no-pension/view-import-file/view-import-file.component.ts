1d76b39f732a7d297e4683517691bfbe7dc50b64
Alvis <aherrera@munily.com>
2023-05-27 11:04:51 -0500

    feat: updated view import

---
+++ ClientApp/src/app/no-pension/view-import-file/view-import-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-import-file',
+  templateUrl: './view-import-file.component.html',
+  styleUrls: ['./view-import-file.component.css']
+})
+export class ViewImportFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
