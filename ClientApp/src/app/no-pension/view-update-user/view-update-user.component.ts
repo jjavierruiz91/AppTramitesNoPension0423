8f352e6e827a0de74eba060f7e0d393176b5ac07
andr11s <aandresherrera@unicesar.edu.co>
2023-05-31 18:15:50 -0500

    feat; add user module

---
+++ ClientApp/src/app/no-pension/view-update-user/view-update-user.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-view-update-user',
+  templateUrl: './view-update-user.component.html',
+  styleUrls: ['./view-update-user.component.css']
+})
+export class ViewUpdateUserComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+}
