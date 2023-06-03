19f22876392a509101e7ccc450e559753d734d92
Alvis <aherrera@munily.com>
2023-06-02 22:16:56 -0500

    feat: update controller

--- Controllers/NopensionController.cs
+++ Controllers/NopensionController.cs
@@ -81,15 +81,19 @@ namespace Aplicativo.net.Controllers
       var ruta = _config.GetSection("routeImportFile").Value + DateTime.Now.Month + Guid.NewGuid().ToString("N") + ".xlsx";
       var staticPath = Path.Combine(path, ruta);
 
-       var validateColumnExcel = fileHeader.ValidateColumns(columnNames,request.Archive); 
-      if(!validateColumnExcel)  return BadRequest(new { 
-        mensaje = "Error, las columnas no coinciden con las del excel ", column= columnNames }); 
+      FileHelper fileHeader = new FileHelper();
+      var validateColumnExcel = fileHeader.ValidateColumns(columnNames, request.Archive);
+      if (!validateColumnExcel) return BadRequest(new
+      {
+        mensaje = "Error, las columnas no coinciden con las del excel ",
+        StatusCode = StatusCodes.Status502BadGateway,
+        column = columnNames
+      });
 
       using (var stream = new FileStream(staticPath, FileMode.Create))
       {
         request.Archive.CopyTo(stream);
       }
-      FileHelper fileHeader = new FileHelper();
 
       var dataExcel = fileHeader.ReadFile(request.Archive);
 
