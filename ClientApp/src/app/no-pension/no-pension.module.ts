91f09b76d60d345967047e321d0a17f2b6ddadc2
andre11s <aandresherrera@unicesar.edu.co>
2023-06-29 19:18:31 -0500

    feat: update module

--- ClientApp/src/app/no-pension/no-pension.module.ts
+++ ClientApp/src/app/no-pension/no-pension.module.ts
@@ -9,10 +9,10 @@ import { MenuModule } from '../menu/menu.module';
 import { ViewUpdateUserComponent } from './view-update-user/view-update-user.component';
 import { ViewUploadFileComponent } from './view-upload-file/view-upload-file.component';
 
-
 @NgModule({
   declarations: [ViewImportFileComponent, ViewUpdateUserComponent, ViewUploadFileComponent],
   imports: [
+
     CommonModule,
     NoPensionRoutingModule,
     NgbTypeaheadModule,
