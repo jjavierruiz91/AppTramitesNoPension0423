e767f5524b9015a06c08f06b5bcf9c5b9a3d8694
andre11s <aandresherrera@unicesar.edu.co>
2023-07-01 17:33:34 -0500

    feat: add new migration

--- Migrations/20230701222554_addnewCOlumns.Designer.cs
+++ Migrations/20230701222554_addnewCOlumns.Designer.cs
@@ -4,14 +4,16 @@ using Aplicativo.net.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Aplicativo.net.Migrations
 {
     [DbContext(typeof(AplicativoContext))]
-    partial class AplicativoContextModelSnapshot : ModelSnapshot
+    [Migration("20230701222554_addnewCOlumns")]
+    partial class addnewCOlumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
